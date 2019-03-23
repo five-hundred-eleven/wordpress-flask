5428f4e484204c6c3bb0304d02a0e5a885eb54de
ekoly <cowley.erik@gmail.com>
2019-03-23 01:28:11 -0500

    updated gitignore

--- build/lib.linux-i686-2.7/pythonwp/config.py
+++ build/lib.linux-i686-2.7/pythonwp/config.py
@@ -4,4 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "XXXXXXXXXXXXXXXXXXXX"
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "mysql:///" + os.path.join(basedir, "app.db")
+    #  SQLALCHEMY_DATABASE_URI="mysql://wpuser:9n&Z5sAR7BFn&Chp@localhost/stromsydb"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
