daf5b92a75aae59d2d4f2837a78f3982a69a079f
ekoly <cowley.erik@gmail.com>
2019-03-22 20:18:07 -0500

    gitignore:

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
-from flask import Flask
 from config import Config
+from flask import Flask
+import os, sys
+
+app_base = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(app_base)
 
 app = Flask(__name__)
 app.config.from_object(Config)
--- app/config.py
+++ app/config.py
@@ -2,4 +2,4 @@ import os
 
 # obviously not the file used in production
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY") or 'WQ3Q5lD6msa8TzZYgWyA'
+    SECRET_KEY = os.environ.get("SECRET_KEY") or 'XXXXXXXXXXXXXXXXXXXX'
