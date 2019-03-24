3cfec62a380196ee8ebc1dec41e4e71237e4cb6d
ekoly <cowley.erik@gmail.com>
2019-03-23 23:15:01 -0500

    added static routes

---
+++ pythonwp/web/StaticRoutes.py
@@ -0,0 +1,22 @@
+from pythonwp import app
+from flask import request, send_from_directory
+
+@app.route('/')
+def getRoot():
+    return send_from_directory("static", "index.html")
+
+@app.route("/js/<path:path>")
+def getJs(path):
+    return send_from_directory("static/js", path)
+
+@app.route("/css/<path:path>")
+def getCss(path):
+    return send_from_directory("static/css", path)
+
+@app.route("/partials/<path:path>")
+def getPartials(path):
+    return send_from_directory("static/partials", path)
+
+@app.route("/img/<path:path>")
+def getImg(path):
+    return send_from_directory("static/img", path)
